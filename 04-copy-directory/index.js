b79618f0da4d2a77bc8066a1d3b476fc774a9b8e
Yury Kalenkou <goldyukol12@gmail.com>
2022-05-24 00:27:30 +0300

    feat: add solution for task #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
+const { join } = require('path');
 
+const FOLDER_DIR = join(__dirname, 'files');
+const FOLDER_DIR_COPY = join(__dirname, 'files-copy');
+
+const updateDir = () => {
+  mkdir(FOLDER_DIR_COPY)
+    .then(() => {
+      readdir(FOLDER_DIR)
+        .then((files) => {
+          files.forEach((fileName) => {
+            copyFile(
+              join(FOLDER_DIR, fileName),
+              join(FOLDER_DIR_COPY, fileName)
+            ).catch((error) => console.warn(error));
+          });
+        })
+        .catch((error) => console.warn(error));
+    })
+    .catch((error) => console.warn(error));
+};
+
+rm(FOLDER_DIR_COPY, { recursive: true })
+  .then(() => updateDir())
+  .catch(() => updateDir());
